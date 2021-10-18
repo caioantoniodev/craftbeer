dfc3f7642b888ffdcab4cbcd9e199af31ed1ed1e
Caio Antonio <caiocichetti08@gmail.com>
2021-10-18 17:00:32 -0300

    chore: initial setup

---
+++ src/main/java/com/academy/beerhouse/craftbeer/CraftBeerApplication.java
@@ -0,0 +1,13 @@
+package com.academy.beerhouse.craftbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftBeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/beerhouse/craftbeer/CraftBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.beerhouse.craftbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
