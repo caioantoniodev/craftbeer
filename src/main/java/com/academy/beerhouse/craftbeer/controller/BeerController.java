972ca608691c8b1303a29d74cf9cc2e0713b3353
Caio Roberto <caio.roberto@sensedia.com>
2021-11-30 14:11:35 -0300

    refactor: update error trace

--- src/main/java/com/academy/beerhouse/craftbeer/controller/BeerController.java
+++ src/main/java/com/academy/beerhouse/craftbeer/controller/BeerController.java
@@ -5,6 +5,7 @@ import com.academy.beerhouse.craftbeer.service.BeerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -25,7 +26,7 @@ public class BeerController {
     }
 
     @GetMapping(path = "{id}")
-    public Mono<Beer> findOne(UUID id) {
+    public Mono<Beer> findOne(@PathVariable UUID id) {
         return beerService.findOne(id);
     }
 }
